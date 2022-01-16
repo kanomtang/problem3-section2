2570c35b48e150df019b36a08713a8f1e5f12303
Rungroj Sonsubanan <rungroj_s@cmu.ac.th>
2022-01-16 18:44:22 +0700

    feat: Implement scraping data from website with given fund name

---
+++ index.js
@@ -0,0 +1,33 @@
+const puppeteer = require('puppeteer')
+
+async function scrape(fundName) {
+   const browser = await puppeteer.launch({})
+   const page = await browser.newPage()
+
+   await page.goto('https://codequiz.azurewebsites.net/')
+   // Click button
+   await (await page.waitForSelector("body > input[type=button]")).click()
+ 
+   // Wait for table finish render
+   await page.waitForSelector("body > table > tbody")
+
+   const fundCollection = await page.evaluate(() => {
+   const rows = document.querySelectorAll('body > table > tbody > tr');
+    return Array.from(rows, row => {
+      const columns = row.querySelectorAll('td');
+      return Array.from(columns, column => column.innerText);
+    });
+  });
+  
+  for (let index = 0; index < fundCollection.length; index++) {
+      if (fundName === fundCollection[index][0]) {
+        // Print Nav of given fund  
+        console.log(fundCollection[index][1])
+        break;
+      }
+  }
+      // Looking given Fund Name
+   browser.close()
+}
+const fundName = process.argv.slice(2,3);
+scrape(fundName.pop())
